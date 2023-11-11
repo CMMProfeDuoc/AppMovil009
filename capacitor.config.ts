11a6ad117f04f7beda086005cef39792598cf80e
Guillemro Villacura <gu.villacura@gmail.conm>
2023-11-11 10:39:15 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tiendaMascotas',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
